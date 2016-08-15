34c18f141bf414538d26020818bb4edec47b7887
Rick Johnson <richardpatrickjohnson@gmail.com>
2016-08-15 13:50:05 -0400

    Add smaller search result list for widget

---
+++ app/controllers/discoversmall.js
@@ -0,0 +1,129 @@
+import _ from 'lodash/lodash';
+import Ember from 'ember';
+import ApplicationController from './application';
+import buildElasticCall from '../utils/build-elastic-call';
+
+export default ApplicationController.extend({
+    queryParams: ['page', 'searchString'],
+    page: 1,
+    size: 10,
+    query: {},
+    searchString: '',
+    displayQueryBaseString: Ember.computed( function() {
+        return buildElasticCall(Ember.$.param(this.searchQuery()));
+    }),
+    collapsedQueryBody: true,
+
+    results: Ember.ArrayProxy.create({content: []}),
+    loading: true,
+
+    init() {
+        //TODO Sort initial results on date_modified
+        this._super(...arguments);
+        let query = this.searchQuery();
+        // TODO Load all previous pages when hitting a page with page > 1
+        // if (this.get('page') != 1) {
+        //   query.from = 0;
+        //   query.size = this.get('page') * this.get('size');
+        // }
+        this.loadPage(query);
+        this.set('debouncedLoadPage', _.debounce(this.loadPage.bind(this), 250));
+    },
+
+    searchQuery() {
+        let query = {
+            q: this.get('searchString') || '*',  // Default to everything
+            from: (this.get('page') - 1) * this.get('size')
+        };
+        return query;
+    },
+
+    //queryBody: Ember.computed('elasticFilter', 'elasticAggregations', function() {
+    //    return {
+    //        'query': this.get('elasticFilter'),
+    //        'aggregations': this.get('elasticDateAggregations')
+    //    };
+    //}),
+
+    loadPage(query=null) {
+        query = query || this.searchQuery();
+        let queryString = Ember.$.param(query);
+        let queryBody = JSON.stringify(this.get('queryBody'));
+        const url = buildElasticCall(queryString);
+        this.set('loading', true);
+        return Ember.$.ajax({
+            'url': url,
+            'crossDomain': true,
+            'type': 'POST',
+            'contentType': 'application/json',
+            'data': queryBody
+        }).then((json) => {
+	    //let queryData = json;
+            let results = json.hits.hits.map((hit) => {
+                // HACK
+                let source = hit._source;
+                source.id = hit._id;
+                source.type = 'elastic-search-result';
+                source.workType = source['@type'];
+                source.contributors = source.contributors.map(contributor => {
+                    return {
+                        familyName: contributor.family_name,
+                        givenName: contributor.given_name,
+                        id: contributor['@id']
+                    };
+                });
+                return source;
+            });
+	    //let queryData = JSON.stringify(results);
+            Ember.run(() => {
+                this.set('loading', false);
+                this.get('results').addObjects(results);
+		//this.get('queryData').addObjects(queryData);
+            });
+        });
+    },
+
+    search() {
+        this.set('page', 1);
+        this.set('loading', true);
+        this.get('results').clear();
+        this.get('debouncedLoadPage')();
+    },
+
+    actions: {
+        toggleCollapsedQueryBody() {
+            this.toggleProperty('collapsedQueryBody');
+        },
+        typing(val, event) {
+            // Ignore all keycodes that do not result in the value changing
+            // 8 == Backspace, 32 == Space
+            if (event.keyCode < 49 && !(event.keyCode === 8 || event.keyCode === 32)) {
+                return;
+            }
+            this.search();
+        },
+        search() {
+            this.search();
+        },
+        filtersChanged(filter) {
+            this.set('elasticFilter', filter);
+            this.search();
+        },
+        next() {
+            // If we don't have full pages then we've hit the end of our search
+            if (this.get('results.length') % this.get('size') !== 0) {
+                return;
+            }
+            this.incrementProperty('page', 1);
+            this.loadPage();
+        },
+        prev() {
+            // No negative pages
+            if (this.get('page') < 1) {
+                return;
+            }
+            this.decrementProperty('page', 1);
+            this.loadPage();
+        }
+    }
+});
